676e8663ff902dae8a5802a11634ff2bc5dcdbb9
Dilshan Ramesh <dilshanramesh81@gmail.com>
2022-01-29 01:56:46 +0530

    auth route and controller added

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,37 @@
+/*
+ * Auth Controller
+ * v1.0.0
+ * Nova Solutions (Pvt) Ltd
+ *
+*/
+
+const httpStatus = require("http-status");
+
+const login = async (req, res, next) => {
+
+}
+
+const logout = async (req, res, next) => {
+
+}
+
+const register = async (req, res, next) => {
+
+}
+
+const loginToken = async (req, res, next) => {
+
+}
+
+const renewRefreshToken = async (req, res, next) => {
+
+}
+
+
+module.exports = {
+    login,
+    logout,
+    register,
+    loginToken,
+    renewRefreshToken
+}
